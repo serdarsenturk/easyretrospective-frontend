638f28183fe09654f411a3e1380f5e2abe1fb401
Serdar <serdarsenturk@windowslive.con>
2021-12-08 00:33:14 +0300

    Pass all components on MyApp function

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/style.css";
+import Header from "./layouts/header";
+import Footer from "./layouts/footer";
+import { Container } from "react-bootstrap";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function Myapp({ Component, pageProps }) {
+  return (
+    <>
+        <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+        crossOrigin="anonymous">
+        </link>
+        
+      <Container>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </Container>
+    </>
+  );
 }
-
-export default MyApp

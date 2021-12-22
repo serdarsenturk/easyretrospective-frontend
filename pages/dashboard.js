59a4d7ebe939b07327926587199b3dfc0d5d743c
Serdar <serdarsenturk@windowslive.con>
2021-12-22 15:00:57 +0300

    Re-work `dashboard` page and add `Tab` for each Team

---
+++ pages/dashboard.js
@@ -0,0 +1,91 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import BoardTab from '../components/board_tab'
+import { useRouter} from 'next/router'
+import { useEffect, useState } from "react";
+import {Col, Row, Tab, Nav} from "react-bootstrap";
+import CreateBoard from '../components/create_board';
+import Pusher from 'pusher-js'
+
+function Dashboard({ boards, member_teams }) {
+  const router = useRouter()
+
+  const [ publicBoardList, setPublicBoardList ] = useState(boards.filter(board => board.team_id === null))
+  var teams = member_teams[0]["teams_members"]
+
+
+  const clickMe = (board) => {
+    router.push(`/boards/${board.code}`)
+  }
+
+  const handleDelete = (board) => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/members/1/boards/${board.code}` , {
+      method: 'DELETE',
+      headers: {
+          'Content-Type': 'application/json',
+      }
+    })
+
+    console.log("Board deleted");
+  }
+
+    return (
+      <>      
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+            <Row>
+                <Col sm={3}>
+                <Nav variant="pills" className="flex-column">
+                    <Nav.Item>
+                    <Nav.Link eventKey="first">Public</Nav.Link>
+                    <CreateBoard team_id=''/>
+                    </Nav.Item>
+                </Nav>
+                </Col>
+                <Col sm={9}>
+                <Tab.Content>
+                    <Tab.Pane eventKey="first">
+                    {publicBoardList.map((board) => (
+                      // <Col key={board.id} onClick={() => clickMe(board)}>
+                      <Col>
+                      <Card style={{ width: '18rem',float:'right' }}>
+                        <Card.Body>
+                          <Card.Title>{board.name}</Card.Title>
+                          <Card.Text>
+                            Board Code: {board.code}
+                            <br/>
+                            Board Date: {board.date}
+                          </Card.Text>
+                          <Col>
+                            <Button variant="primary">URL</Button>
+                            <Button style={ { margin: '1rem'}} variant="primary" onClick={() => handleDelete(board)}>DELETE</Button>
+                            <Button style={ { margin: '1rem'}} variant="primary" onClick={() => clickMe(board)}>GOTO</Button>
+                          </Col>
+                        </Card.Body>
+                      </Card>
+                      </Col>
+                    ))}                        
+                    </Tab.Pane>
+                </Tab.Content>
+                </Col>
+            </Row>
+            </Tab.Container>
+					<Row>
+            {teams.map((team) =>(
+              <BoardTab team={team}/>
+            ))}
+					</Row>
+      </>
+  )
+}
+
+Dashboard.getInitialProps = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/members/1/boards`)
+  const boards = await res.json()
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/members/1/teams`)
+  const member_teams = await response.json()
+
+  return { boards: boards , member_teams: member_teams}
+}
+
+export default Dashboard

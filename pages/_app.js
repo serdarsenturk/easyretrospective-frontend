1ead9cba9255565e69164c4ae061a4301a8b9892
Serdar <serdarsenturk@windowslive.con>
2022-03-06 07:28:41 +0300

    Remove Container component and use <div> element instead of Container

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/style.css";
 import Header from "./layouts/header";
 import Footer from "./layouts/footer";
-import { Container } from "react-bootstrap";
 
 export default function Myapp({ Component, pageProps }) {
   return (
     <>
-      <Container>
-        <Header />
+      <div>
+        <Header/>
         <Component {...pageProps} />
-        <Footer />
-      </Container>
+        <Footer/>
+      </div>
     </>
   );
 }

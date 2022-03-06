9d147bf3540fa3534738729c57a022a8ca425f21
Serdar <serdarsenturk@windowslive.con>
2022-03-06 07:28:12 +0300

    Remove bootstrap CDN link

--- pages/_app.js
+++ pages/_app.js
@@ -7,13 +7,6 @@ import { Container } from "react-bootstrap";
 export default function Myapp({ Component, pageProps }) {
   return (
     <>
-        <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
-        crossOrigin="anonymous">
-        </link>
-        
       <Container>
         <Header />
         <Component {...pageProps} />

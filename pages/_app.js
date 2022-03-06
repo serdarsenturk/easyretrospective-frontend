b5f4758739a0edeb4645ed09d0c229cf9f3dfa96
Serdar <serdarsenturk@windowslive.con>
2022-03-06 07:29:34 +0300

    Import `fontawesome` font package

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/style.css";
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
 import Header from "./layouts/header";
 import Footer from "./layouts/footer";
+config.autoAddCss = false; 
 
 export default function Myapp({ Component, pageProps }) {
   return (

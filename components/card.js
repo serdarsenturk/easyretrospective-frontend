bb1962dd7154414e9eeabe83f130210187eb3253
Serdar <serdarsenturk@windowslive.con>
2022-03-07 23:35:25 +0300

    Remove bootstrap `card` component

--- components/card.js
+++ components/card.js
@@ -8,7 +8,6 @@ export default class Cards extends Component{
     constructor(props) {
         super(props);
         this.state = {card: this.props.card, card_content: ''};
-
     }
 
     deleteCard = (event, card) => {
@@ -20,28 +19,13 @@ export default class Cards extends Component{
                 'Content-Type': 'application/json',
             }
           })
-      
-          console.log("Column deleted");
     }
 
     render(){
         return (
         <>
-            <Col>
-            <Card style={{ width: '17rem',float:'center'}} className="mx-auto my-2">
-                <Card.Body>
-                    <Card style={{ width: '15rem',float:'center' }} key={this.props.card.id}>
                     <Card.Body>
-                        <Card.Title>Card Id: {this.state.card.id}</Card.Title>
-                        <Card.Text>
-                            <CardContent card ={this.state.card} card_content={this.state.card.content} column_id={this.props.column_id} member_id={this.props.member_id} board_code={this.props.board_code}/>
-                        </Card.Text>
-                        <Button variant="primary" onClick={(event) => this.deleteCard(event, this.state.card)}>Delete Card</Button>
                     </Card.Body>
-                    </Card>
-                </Card.Body>
-            </Card>
-            </Col>
         </>
         )
     }

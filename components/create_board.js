6f1207a2b48a6615062cc453e36f75f9caada866
Serdar <serdarsenturk@windowslive.con>
2022-03-08 00:18:07 +0300

    Add whitespace

--- components/create_board.js
+++ components/create_board.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export default class CreateBoard extends React.Component{
     constructor(props) {

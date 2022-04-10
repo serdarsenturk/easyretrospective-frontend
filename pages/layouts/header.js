038046b53f18af67c4b548d7391b6b58f5ebc023
Serdar <serdarsenturk@windowslive.con>
2022-04-10 19:43:11 +0300

    Remove old header structure

--- pages/layouts/header.js
+++ pages/layouts/header.js
@@ -4,23 +4,6 @@ import { faColumns, faPlus, faHome, faTachometer } from "@fortawesome/free-solid
 export default function Header() {
     return (
       <>
-        <header className="masthead">
-            <div className="boards-menu">
-
-                <nav>
-                  <a className="boards-btn btn" href="/"><FontAwesomeIcon icon={faHome} aria-hidden="true"/> Home</a>
-                </nav>
-
-                <nav>
-                  <a className="boards-btn btn" href="/dashboard"><FontAwesomeIcon icon={faTachometer} aria-hidden="true"/> Dashboard</a>
-                </nav>
-            </div>
-
-            <div className="logo">
-                <i><FontAwesomeIcon icon={faColumns}/>EasyRetrospective</i>
-            </div>
-            
-        </header>
       </>
     );
   }

5070b1018eccdabdfbaf8bc2340599703d647c7a
Serdar <serdarsenturk@windowslive.con>
2022-04-12 07:39:44 +0300

    Add blank line

--- components/board_container.js
+++ components/board_container.js
@@ -5,6 +5,7 @@ import { withRouter } from 'next/router'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Col, Row, Card, Button, Container, Dropdown } from "react-bootstrap";
+
 class BoardContainer extends Component{
     constructor(props) {
         super(props);

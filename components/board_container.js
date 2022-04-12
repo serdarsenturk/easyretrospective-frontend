98244913d4bbe6f56d62292f149474703fe53a39
Serdar <serdarsenturk@windowslive.con>
2022-04-12 07:25:56 +0300

    Import and configure Pusher

--- components/board_container.js
+++ components/board_container.js
@@ -1,4 +1,10 @@
+import Pusher from 'pusher-js'
 import { withRouter } from 'next/router'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Col, Row, Card, Button, Container, Dropdown } from "react-bootstrap";
+      Pusher.logToConsole=process.env.NEXT_PUBLIC_PUSHER_DEBUGGING
+      this.pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
+          cluster: 'eu',
+          encrypted: true
+      });

6b60b89f15ffbcb1d23857b7342732bb726a583a
Serdar <serdarsenturk@windowslive.con>
2022-04-12 07:25:09 +0300

    Import withRouter function

--- components/board_container.js
+++ components/board_container.js
@@ -1,3 +1,4 @@
+import { withRouter } from 'next/router'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Col, Row, Card, Button, Container, Dropdown } from "react-bootstrap";

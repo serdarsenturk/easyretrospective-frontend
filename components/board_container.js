b1012b76b1ce407a64aacd728fbaf12a66024de7
Serdar <serdarsenturk@windowslive.con>
2022-04-12 07:58:15 +0300

    Remove Pusher events, package and unsubscribe functions to re-work another branch

--- components/board_container.js
+++ components/board_container.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import CreateBoard from "./create_board";
-import Pusher from 'pusher-js'
 import { withRouter } from 'next/router'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
@@ -14,8 +13,6 @@ class BoardContainer extends Component{
     }
     
     componentDidMount(){
-      Pusher.logToConsole=process.env.NEXT_PUBLIC_PUSHER_DEBUGGING
-
       if (this.state.team_id != null){
         fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/teams/${this.state.team_id}/boards` , {
             method: 'GET',
@@ -36,35 +33,6 @@ class BoardContainer extends Component{
                 this.setState({boards: boards})
           })
       }
-        
-      this.pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY, {
-          cluster: 'eu',
-          encrypted: true
-      });
-
-      this.channel_dashboard = this.pusher.subscribe(`member-${this.state.member_id}`);
-
-      this.channel_dashboard.bind('board-created', function(created_board) {
-          this.state.boards.push(created_board);
-        })
-
-      this.channel_dashboard.bind('board-deleted', function(deleted_board) {
-          this.setState({boards: this.state.boards.filter(board => board.code != deleted_board.code)});
-        })
-
-      this.channel_dashboard.bind('board-updated', function(updated_board) {
-        const boardList = this.state.boards;
-        console.log("serdar")
-        const boardIndex = boardList.findIndex(board => board.code === updated_board.code);
-        boardList[boardIndex].name = updated_board.name;
-
-        if(boardList[boardIndex].team_id){
-          this.setState({boards: boardList.filter(board => board.team_id == boardList[boardIndex].team_id)});
-        }
-        else {
-          this.setState({boards: boardList.filter(board => board.team_id === null)});               
-        }
-      })
     }
 
     handleDelete = (board, member_id) => {
@@ -78,12 +46,6 @@ class BoardContainer extends Component{
     
     handleClick = (board_code) => {
       this.router.push(`/boards/${board_code}`)
-    }
-
-    componentWillUnmount() {
-      this.channel_dashboard.unbind();
-
-      this.pusher.unsubscribe(this.channel_dashboard);
     }
 
     render(){

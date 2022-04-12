5d34203d31d7990cae73d1e498de324361abdd5e
Serdar <serdarsenturk@windowslive.con>
2022-04-12 07:57:17 +0300

    Remove faAngleDown icon

--- components/board_container.js
+++ components/board_container.js
@@ -3,7 +3,7 @@ import CreateBoard from "./create_board";
 import Pusher from 'pusher-js'
 import { withRouter } from 'next/router'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Col, Row, Card, Button, Container, Dropdown } from "react-bootstrap";
 
 class BoardContainer extends Component{

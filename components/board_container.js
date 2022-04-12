65a4c84b252a751c8b67e686167bb7ce312dc69b
Serdar <serdarsenturk@windowslive.con>
2022-04-12 07:28:08 +0300

    Create `team_id`, `team_name`, `boards` and `member_id` states

--- components/board_container.js
+++ components/board_container.js
@@ -8,6 +8,7 @@ import { Col, Row, Card, Button, Container, Dropdown } from "react-bootstrap";
 class BoardContainer extends Component{
     constructor(props) {
         super(props);
+        this.state = {team_id: this.props.team_id, team_name: '', boards: [], member_id: this.props.member_id};
     }
     
       Pusher.logToConsole=process.env.NEXT_PUBLIC_PUSHER_DEBUGGING

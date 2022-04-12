f1a7234d28858589e1a534368ed7ce39ada1d34c
Serdar <serdarsenturk@windowslive.con>
2022-04-12 07:24:55 +0300

    Import Col, Row, Card, Button, Container, Dropdown bootstrap components

--- components/board_container.js
+++ components/board_container.js
@@ -1,2 +1,3 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Col, Row, Card, Button, Container, Dropdown } from "react-bootstrap";

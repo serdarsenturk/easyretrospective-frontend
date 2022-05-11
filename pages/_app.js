eb22508b6d62f91bf5412d457b4a36258f214ca0
Serdar <serdarsenturk@windowslive.com>
2022-05-11 04:14:42 +0300

    Add initAuth function to initialize firebase authentication configuration

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import "../styles/style.css";
 import Header from "./layouts/header";
 import Footer from "./layouts/footer";
 import { Container } from "react-bootstrap";
+import initAuth from '../utils/initAuth'
+
+initAuth()
 
 export default function Myapp({ Component, pageProps }) {
   return (

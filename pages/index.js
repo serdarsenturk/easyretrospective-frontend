84aef6e6db00c80936a147db4c095bacc8ecd1c1
Serdar <serdarsenturk@windowslive.com>
2022-05-13 21:32:15 +0300

    Create simple landing interface with sign-in button

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,47 @@
-export default function Home() {
+import React from 'react'
+import Link from 'next/link'
+import { Button } from "react-bootstrap";
+import {
+  useAuthUser,
+  withAuthUser,
+  withAuthUserTokenSSR,
+} from 'next-firebase-auth'
+
+const Home = () => {
+  const AuthUser = useAuthUser()
+  const email = AuthUser.email;
+  const signOut = AuthUser.signOut
+
   return (
-      <main>
-        <h1>
-          <a>Welcome to Easyretrospective</a></h1>
-      </main>
+    <>
+    <div>
+    {email ? (
+      <>
+        <p>Signed in as {email}</p>
+        <Button
+        type="button"
+        onClick={() => {
+          signOut()
+        }}
+        >Sign out </Button>
+      </>
+    ) : (
+      <>
+        <p>You are not signed in.</p>
+        <Link href="/auth">
+          <a>
+            <Button type="button">
+              Sign in
+            </Button>
+          </a>
+        </Link>
+      </>
+    )}
+  </div>
+    </>
   )
 }
+
+export const getServerSideProps = withAuthUserTokenSSR()()
+
+export default withAuthUser()(Home)

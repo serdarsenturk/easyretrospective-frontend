8a58e54ffafd76b5a79d475d8ce9040c6b915369
Serdar <serdarsenturk@windowslive.com>
2022-05-23 17:34:08 +0300

    Update

--- components/firebase_auth.js
+++ components/firebase_auth.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase/compat/app';
-import 'firebase/auth';
+import 'firebase/compat/auth';
 
 const firebaseAuthConfig = {
   signInFlow: 'popup',
